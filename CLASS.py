73b24fbe598e45df6ba393ed09b6ead106b82285
Wenbin Zhu <zwbdmmfly@gmail.com>
2017-10-21 22:36:03 -0400

    this is the python tutorials I have made

---
+++ CLASS.py
@@ -0,0 +1,166 @@
+#Objective class
+##ahout the class:
+class Employee:
+    number_of_emps = 0
+
+    raise_amount = 1.04
+
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + '.' + last + '@company.edu'
+        Employee.number_of_emps += 1
+
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amount)
+
+    @classmethod
+    def set_raise_amount(cls, amount):
+        cls.raise_amount = amount
+
+    @classmethod
+    def from_string(cls, emp_str):
+        first, last, pay = emp_str.split('-')
+        return cls(first, last, pay)
+
+    @staticmethod
+    def is_workday(day):
+        if day.weekday() == 5 or day.weekday() == 6:
+            return False
+        return True
+
+    def __repr__(self):
+        return "Employee('{}', '{}', {})".format(self.first, self.last, self.pay)
+
+    def __str__(self):
+        return '{} - {}'.format(self.fullname(), self.email)
+
+    def __add__(self, other):
+        return self.pay + other.pay
+
+
+emp_1 = Employee('Michael', 'Jackson',50000)
+emp_2 = Employee('Larry', 'Peter',60000)
+
+print(emp_1 + emp_2)
+
+print(emp_1)
+print(repr(emp_1))
+print(emp_1.__repr__())
+print(str(emp_1))
+print(emp_1.__str__())
+
+#print(emp_1.fullname())
+#print(Employee.fullname(emp_2))
+
+emp_1.apply_raise()
+print(emp_1.pay)
+print(emp_1.raise_amount)
+print(emp_2.raise_amount)
+print(Employee.raise_amount)
+
+#use __dict__ to see the all the stuff inside.
+print(emp_1.__dict__)
+print(Employee.__dict__)
+
+#reset the parameter for raise amount for a class
+print(emp_1.raise_amount)
+print(Employee.raise_amount)
+
+#reset the parameter for raise amount for a instance
+emp_1.raise_amount = 1.5
+print(emp_1.raise_amount)
+print(Employee.raise_amount)
+print(emp_1.__dict__)
+
+print(Employee.number_of_emps)
+
+#about the class method
+Employee.set_raise_amount(3)
+print(emp_2.raise_amount)
+print(emp_1.raise_amount)
+
+emp_str_1 = 'Cece-Furtner-10000'
+emp_str_2 = 'Frank-Liu-10000'
+emp_str_3 = 'Bangde-Liu-20000'
+
+new_emp_1 = Employee.from_string(emp_str_1)
+print(new_emp_1.email)
+print(new_emp_1.fullname())
+
+#about the staticmethod
+import datetime
+my_date = datetime.date(2017,10,22)
+print(Employee.is_workday(my_date))
+
+##############################################################################
+##########################      Inheritence          ##########################
+##############################################################################
+class Developer(Employee):
+    raise_amount =1.1
+
+    def __init__(self, first, last, pay, prog_lang):
+        super().__init__(first, last, pay)
+        self.prog_lang = prog_lang
+
+
+dev_1 = Developer('Jack', 'Ma', 20000, 'Python')
+dev_2 = Developer('Jeff', 'Wu', 30000, 'C++')
+
+print(dev_2.prog_lang)
+
+
+class Manager(Employee):
+
+    def __init__(self, first, last, pay, employees = None):
+        super().__init__(first, last, pay)
+        if employees == None:
+            self.employees = []
+        else:
+            self.employees = employees
+
+    def add_emp(self, emp):
+        if emp not in self.employees:
+            self.employees.append(emp)
+
+    def remove_emp(self, emp):
+        if emp in self.employees:
+            self.employees.remove(emp)
+
+    def print_emps(self):
+        for emp in self.employees:
+            print('-->', emp.fullname())
+
+mgr_1 = Manager('Wenbin','Zhu', '90000', employees = [dev_1])
+print(mgr_1.email)
+
+mgr_1.add_emp(dev_2)
+
+mgr_1.print_emps()
+
+print(isinstance(mgr_1, Manager)) #True
+print(isinstance(mgr_1, Employee)) #True
+print(isinstance(mgr_1, Developer)) #False
+
+print(issubclass(Manager, Developer)) #FALSE
+print(issubclass(Manager, Employee)) #TRUE
+
+####################################################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ python_tutorials.py
@@ -0,0 +1,379 @@
+#this is the tutorials in cs231n
+from math import sqrt
+t = True
+f = False
+print(type(t)) # Prints "<class 'bool'>"
+print(t and f) # Logical AND; prints "False"
+print(t or f)  # Logical OR; prints "True"
+print(not t)   # Logical NOT; prints "False"
+print(t != f)  # Logical XOR; prints "True"
+
+hw12 = '%s %s %d' % (hello, world, 12)
+
+##List
+xs = [3, 1, 2]    # Create a list
+print(xs, xs[2])  # Prints "[3, 1, 2] 2"
+print(xs[-1])     # Negative indices count from the end of the list; prints "2"
+xs[2] = 'foo'     # Lists can contain elements of different types
+print(xs)         # Prints "[3, 1, 'foo']"
+xs.append('bar')  # Add a new element to the end of the list
+print(xs)         # Prints "[3, 1, 'foo', 'bar']"
+x = xs.pop()      # Remove and return the last element of the list
+print(x, xs)      # Prints "bar [3, 1, 'foo']"
+
+##slicing
+nums = list(range(5))     # range is a built-in function that creates a list of integers
+print(nums)               # Prints "[0, 1, 2, 3, 4]"
+print(nums[2:4])          # Get a slice from index 2 to 4 (exclusive); prints "[2, 3]"
+print(nums[2:])           # Get a slice from index 2 to the end; prints "[2, 3, 4]"
+print(nums[:2])           # Get a slice from the start to index 2 (exclusive); prints "[0, 1]"
+print(nums[:])            # Get a slice of the whole list; prints "[0, 1, 2, 3, 4]"
+print(nums[:-1])          # Slice indices can be negative; prints "[0, 1, 2, 3]"
+nums[2:4] = [8, 9]        # Assign a new sublist to a slice
+print(nums)               # Prints "[0, 1, 8, 9, 4]"
+
+##loops
+animals = ['cat', 'dog', 'monkey']
+for animal in animals:
+    print(animal)
+
+animals = ['cat', 'dog', 'monkey']
+for idx, animal in enumerate(animals):
+    print('#%d: %s' % (idx, animal))
+# Prints "#1: cat", "#2: dog", "#3: monkey", each on its own line
+
+##list creating
+nums = [0, 1, 2, 3, 4]
+squares = []
+for x in nums:
+    squares.append(x ** 2)
+print(squares)
+
+nums = [0, 1, 2, 3, 4]
+squares = [x ** 2 for x in nums]
+print(squares)   # Prints [0, 1, 4, 9, 16]
+
+nums = [0, 1, 2, 3, 4]
+even_squares = [x ** 2 for x in nums if x % 2 == 0]
+print(even_squares)  # Prints "[0, 4, 16]"
+
+##dictionary
+d = {'cat': 'cute', 'dog': 'furry'}  # Create a new dictionary with some data
+print(d['cat'])       # Get an entry from a dictionary; prints "cute"
+print('cat' in d)     # Check if a dictionary has a given key; prints "True"
+d['fish'] = 'wet'     # Set an entry in a dictionary
+print(d['fish'])      # Prints "wet"
+# print(d['monkey'])  # KeyError: 'monkey' not a key of d
+print(d.get('monkey', 'N/A'))  # Get an element with a default; prints "N/A"
+print(d.get('fish', 'N/A'))    # Get an element with a default; prints "wet"
+del d['fish']         # Remove an element from a dictionary
+print(d.get('fish', 'N/A')) # "fish" is no longer a key; prints "N/A"
+
+
+
+d = {'person': 2, 'cat': 4, 'spider': 8}
+for animal in d:
+    legs = d[animal]
+    print(animal)
+    print(legs)
+    print('A %s has %d legs' % (animal, legs))
+# Prints "A person has 2 legs", "A cat has 4 legs", "A spider has 8 legs"
+
+d = {'person': 2, 'cat': 4, 'spider': 8}
+for animal, legs in d.items():
+    print('A %s has %d legs' % (animal, legs))
+# Prints "A person has 2 legs", "A cat has 4 legs", "A spider has 8 legs"
+
+#making a dictionary in such a comprehension
+nums = [0, 1, 2, 3, 4]
+even_num_to_square = {x: x ** 2 for x in nums if x % 2 == 0}
+print(even_num_to_square)  # Prints "{0: 0, 2: 4, 4: 16}"
+
+
+animals = {'cat', 'dog'}
+print('cat' in animals)   # Check if an element is in a set; prints "True"
+print('fish' in animals)  # prints "False"
+animals.add('fish')       # Add an element to a set
+print('fish' in animals)  # Prints "True"
+print(len(animals))       # Number of elements in a set; prints "3"
+animals.add('cat')        # Adding an element that is already in the set does nothing
+print(len(animals))       # Prints "3"
+animals.remove('cat')     # Remove an element from a set
+print(len(animals))       # Prints "2"
+
+##looping over set
+animals = {'cat', 'dog', 'fish'}
+for idx, animal in enumerate(animals):
+    print('#%d: %s' % (idx + 1, animal))# Prints "#1: fish", "#2: dog", "#3: cat"
+
+##some notes on enumerate, list(enumerate(xx)) will return a tuple
+test = list(enumerate(animals))
+print(test[1])
+
+nums = {int(sqrt(x)) for x in range(30)}
+print(nums)  # Prints "{0, 1, 2, 3, 4, 5}"
+print(sqrt(range(30)))
+
+#Tuples::
+d = {(x, x + 1): x for x in range(10)}  # Create a dictionary with tuple keys
+t = (5, 6)        # Create a tuple
+print(type(t))    # Prints "<class 'tuple'>"
+print(d[t])       # Prints "5"
+print(d[(1, 2)])  # Prints "1"
+
+#funcitons definition
+def sign(x):
+    if x > 0:
+        return 'positive'
+    elif x < 0:
+        return 'negative'
+    else:
+        return 'zero'
+
+
+for x in [-1, 0, 1]:
+    print(sign(x))
+# Prints "negative", "zero", "positive"
+
+##this is how a function take a value.
+def hello(name, loud=False):
+    if loud:
+        print('HELLO, %s!' % name.upper())
+    else:
+        print('Hello, %s' % name)
+
+hello('Bob') # Prints "Hello, Bob"
+hello('Fred', loud=True)  # Prints "HELLO, FRED!"
+
+##ahout the class:
+class Employee:
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + '.' + last + '@company.edu'
+
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+
+emp_1 = Employee('Michael', 'Jackson',50000)
+emp_2 = Employee('Larry', 'Peter',60000)
+
+print(emp_1.fullname())
+print(Employee.fullname(emp_2))
+
+#Numpy Arrarys
+import numpy as np
+
+a = np.array([1, 2, 3])   # Create a rank 1 array
+print(type(a))            # Prints "<class 'numpy.ndarray'>"
+print(a.shape)            # Prints "(3,)"
+print(a[0], a[1], a[2])   # Prints "1 2 3"
+a[0] = 5                  # Change an element of the array
+print(a)                  # Prints "[5, 2, 3]"
+
+b = np.array([[1,2,3],[4,5,6]])    # Create a rank 2 array
+print(b)
+print(b.shape)                     # Prints "(2, 3)"
+print(b[0, 0], b[0, 1], b[1, 0])   # Prints "1 2 4"
+
+a = np.zeros((2,2))   # Create an array of all zeros
+print(a)              # Prints "[[ 0.  0.]
+                      #          [ 0.  0.]]"
+
+b = np.ones((1,2))    # Create an array of all ones
+print(b)              # Prints "[[ 1.  1.]]"
+
+c = np.full((2,2), 7)  # Create a constant array
+print(c)               # Prints "[[ 7.  7.]
+
+                       #          [ 7.  7.]]"
+
+d = np.eye(2)         # Create a 2x2 identity matrix
+print(d)              # Prints "[[ 1.  0.]
+                      #          [ 0.  1.]]"
+
+e = np.random.random((2,2))  # Create an array filled with random values
+print(e)                     # Might print "[[ 0.91940167  0.08143941]
+                             #               [ 0.68744134  0.87236687]]"
+#how to use arange, similar to seq in r
+np.arange(10)
+np.arange(2, 10, dtype=np.float)
+np.arange(2, 3, 0.1)
+
+#predetermined the number we want from a to b
+np.linspace(1., 4., 6)
+
+##############################################################################
+##############################################################################
+# Create the following rank 2 array with shape (3, 4)
+# [[ 1  2  3  4]
+#  [ 5  6  7  8]
+#  [ 9 10 11 12]]
+a = np.array([[1,2,3,4], [5,6,7,8], [9,10,11,12]])
+import numpy as np
+# Two ways of accessing the data in the middle row of the array.
+# Mixing integer indexing with slices yields an array of lower rank,
+# while using only slices yields an array of the same rank as the
+# original array:
+row_r1 = a[1, :]    # Rank 1 view of the second row of a
+row_r2 = a[1:2, :]  # Rank 2 view of the second row of a
+print(row_r1, row_r1.shape)  # Prints "[5 6 7 8] (4,)"
+print(row_r2, row_r2.shape)  # Prints "[[5 6 7 8]] (1, 4)"
+
+# We can make the same distinction when accessing columns of an array:
+col_r1 = a[:, 1]
+col_r2 = a[:, 1:2]
+print(col_r1, col_r1.shape)  # Prints "[ 2  6 10] (3,)"
+print(col_r2, col_r2.shape)  # Prints "[[ 2]
+                             #          [ 6]
+                             #          [10]] (3, 1)"
+
+
+a = np.array([[1,2], [3, 4], [5, 6]])
+print(a)
+#this is just put the index in the first part and second part
+# An example of integer array indexing.
+# The returned array will have shape (3,) and
+print(a[[0, 1, 2], [0, 1, 0]])  # Prints "[1 4 5]"
+print(a[[0,1,2], [0,1,0]])
+
+# The above example of integer array indexing is equivalent to this:
+print(np.array([a[0, 0], a[1, 1], a[2, 0]]))  # Prints "[1 4 5]"
+
+# When using integer array indexing, you can reuse the same
+# element from the source array:
+print(a[[0, 0], [1, 1]])  # Prints "[2 2]"
+
+# Equivalent to the previous integer array indexing example
+print(np.array([a[0, 1], a[0, 1]]))  # Prints "[2 2]"
+
+
+'''
+this is for the cell modification in a array
+'''
+
+# Create a new array from which we will select elements
+a = np.array([[1,2,3], [4,5,6], [7,8,9], [10, 11, 12]])
+
+print(a)  # prints "array([[ 1,  2,  3],
+          #                [ 4,  5,  6],
+          #                [ 7,  8,  9],
+          #                [10, 11, 12]])"
+
+# Create an array of indices
+b = np.array([0, 2, 0, 1])
+
+# Select one element from each row of a using the indices in b
+print(a[np.arange(4), b])  # Prints "[ 1  6  7 11]"
+
+# Mutate one element from each row of a using the indices in b
+a[np.arange(4), b] += 10
+
+print(a)  # prints "array([[11,  2,  3],
+          #                [ 4,  5, 16],
+          #                [17,  8,  9],
+          #                [10, 21, 12]])
+
+"""
+how to work with boolean
+"""
+
+
+
+a = np.array([[1,2], [3, 4], [5, 6]])
+
+bool_idx = (a > 2)   # Find the elements of a that are bigger than 2;
+                     # this returns a numpy array of Booleans of the same
+                     # shape as a, where each slot of bool_idx tells
+                     # whether that element of a is > 2.
+
+print(bool_idx)      # Prints "[[False False]
+                     #          [ True  True]
+                     #          [ True  True]]"
+
+# We use boolean array indexing to construct a rank 1 array
+# consisting of the elements of a corresponding to the True values
+# of bool_idx
+print(a[bool_idx])  # Prints "[3 4 5 6]"
+
+# We can do all of the above in a single concise statement:
+print(a[a > 2])     # Prints "[3 4 5 6]"
+print(a[a>2].shape)
+
+##############################################################################
+##########################      Datatypes           ##########################
+##############################################################################
+
+x = np.array([1, 2])   # Let numpy choose the datatype
+print(x.dtype)         # Prints "int64"
+
+x = np.array([1.0, 2.0])   # Let numpy choose the datatype
+print(x.dtype)             # Prints "float64"
+
+x = np.array([1, 2], dtype=np.int64)   # Force a particular datatype
+print(x.dtype)                         # Prints "int64"
+
+
+##############################################################################
+##########################      Array Math          ##########################
+##############################################################################
+
+x = np.array([[1,2],[3,4]], dtype=np.float64)
+y = np.array([[5,6],[7,8]], dtype=np.float64)
+
+# Elementwise sum; both produce the array
+# [[ 6.0  8.0]
+#  [10.0 12.0]]
+print(x + y)
+print(np.add(x, y))
+
+# Elementwise difference; both produce the array
+# [[-4.0 -4.0]
+#  [-4.0 -4.0]]
+print(x - y)
+print(np.subtract(x, y))
+
+# Elementwise product; both produce the array
+# [[ 5.0 12.0]
+#  [21.0 32.0]]
+print(x * y)
+print(np.multiply(x, y))
+
+# Elementwise division; both produce the array
+# [[ 0.2         0.33333333]
+#  [ 0.42857143  0.5       ]]
+print(x / y)
+print(np.divide(x, y))
+
+# Elementwise square root; produces the array
+# [[ 1.          1.41421356]
+#  [ 1.73205081  2.        ]]
+print(np.sqrt(x))
+
+
+##############################################################################
+##########################      Dot product          ##########################
+##############################################################################
+
+x = np.array([[1,2],[3,4]])
+y = np.array([[5,6],[7,8]])
+
+v = np.array([9,10])
+w = np.array([11, 12])
+
+# Inner product of vectors; both produce 219
+print(v.dot(w))
+print(np.dot(v, w))
+
+# Matrix / vector product; both produce the rank 1 array [29 67]
+# x.dot will change the v into a vertical vector 2*1
+print(x.dot(v))
+print(np.dot(x, v))
+
+# Matrix / matrix product; both produce the rank 2 array
+# [[19 22]
+#  [43 50]]
+print(x.dot(y))
+print(np.dot(x, y))
+
